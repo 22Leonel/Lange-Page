0a47a861098fe4df3c8e53e7ff6bba816264ae3a
Manuel Leonel <manuelleonel73@gmail.com>
2022-10-25 20:49:58 +0100

    Landing page

---
+++ _js/funtions.js
@@ -0,0 +1,13 @@
+
+
+var igm = document.querySelector('#phone')
+
+function imgTrocar(e)
+{
+   igm.src = e
+}
+
+function circleColor(c){
+    const circle= document.querySelector('.circle')
+    circle.style.background = c;
+}
